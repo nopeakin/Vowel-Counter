b09d134ad80a5cd5d9835a3dd2d58899ea223c11
Pete Eakin <65780891+nopeakin@users.noreply.github.com>
2020-06-19 17:12:53 -0700

    Add files via upload

---
+++ vowel_counter.py
@@ -0,0 +1,8 @@
+def vowel_counter():
+    string = "Insert string here".lower()
+    a=string.count("a")
+    e=string.count("e")
+    i=string.count("i")
+    o=string.count("o")
+    u=string.count("u")
+    print(a+e+i+o+u)
